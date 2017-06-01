f336b09e4ee61f3629126370f099f2a24d24b5d1
teodorapavlova <theodorapavlova@gmail.com>
2017-06-01 11:02:00 +0300

    Second commit

---
+++ finalExam/src/test/java/exam/finalExam/AllTest.java
@@ -0,0 +1,14 @@
+package exam.finalExam;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	LogInPageTest.class,
+	ProductPageTest.class
+})
+public class AllTest {
+
+}
